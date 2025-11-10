8d63d8b7e12ff63de3f3ad9a98c67c685a3d40b5
anaochrym <ana.ochrym.almeida@escola.pr.gov.br>
2025-11-10 09:22:51 -0300

    imc na pagina

--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,19 @@
 var titulo = document.querySelector(".titulo");
- titulo.textContent = "Aparencia Nutricionista";
+titulo.textContent = "Aparecida Nutricionista";
 
-/= IMC = massa/altura^2
+/*  IMC = massa/altura^2    */
 var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = document.querySelector(.info-peso");
-var peso = tdPeso. textoContent;
-var tdaltura = document.querySelector(".info-altura");
-var altura = tdaltura.textoContent;
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+var tdAltura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 var imc = peso / (altura*altura);
 
-console.log(paciente); //tr
-console.log(tdPeso); // td que tem peso
-console.log(peso); //valor do peso
-console.log(altura); //valor da altura
+console.log(paciente);  //tr
+console.log(tdPeso);    // td que tem peso
+console.log(tdAltura);  //td que tem altura
+console.log(peso);      //valor do peso
+console.log(altura);    //valor da altura
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
